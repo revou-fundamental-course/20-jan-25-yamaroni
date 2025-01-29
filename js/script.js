bed871d27ba07a9153ebf14080f6fb5c121953df
Yama Roni <yamaroni8@gmail.com>
2025-01-29 20:11:48 +0700

    update js & responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // Ini File Javascript
+
+console.log('Hello World!');
+
+function calculateBmi() {
+    let resultBmi = document.getElementById('result-bmi');
+    let weight = document.getElementById('input-berat-badan').value;
+    let height = document.getElementById('input-tinggi-badan').value;
+    let age = document.getElementById('input-usia').value;
+
+    if (weight != '' && height != '' && age != '') {
+        let bmi = (weight / ((height / 100) ** 2)).toFixed(2);
+        resultBmi.textContent = bmi;
+    } else {
+        alert('Please fill in all fields');
+    }
+}
